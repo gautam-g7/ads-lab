26b63999060f9ce853f4703283c4db61d5426d22
gautam-g7 <gautamgirish5499@gmail.com>
2026-09-23 14:30:33 +0530

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,20 +1,35 @@
-#include <stdio.h>
-#define Size 5
-int main();
-int queue[Size];
-int front = -1;
-int rear = -1;
-
-void enqueue(int ele) {
-    if (rear == Size - 1) {
-        printf("Queue is full\n");
-    } 
-    else if (front == -1 && rear == -1) {
-        front = rear = 0;
-        queue[rear] = ele;
-    } 
-    else {
-        rear++;
-        queue[rear] = ele;
-    }
+#include<stdio.h>
+#define size 5
+int queue[size];
+int front=-1;
+int rear=-1;
+void enqueue(int ele)
+{
+if(rear==size-1)
+printf("queue is full");
+else if(front==-1 && rear==-1)
+{
+front=rear=0;
+queue[rear]=ele;
+rear++;
+}
+else
+{
+queue[rear]=ele;
+rear++;
+}
+}
+void display()
+{
+printf("\nqueue is");
+for(int i=front;i<=rear;i++)
+printf("\n%d",queue[i]);
+}
+int main() 
+{
+enqueue(10);
+enqueue(20);
+enqueue(30);
+enqueue(40);
+display(); 
 }
